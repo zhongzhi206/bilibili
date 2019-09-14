a7fc1fd5cf3eee7b776fe0b96d5273dfe79534d0
zhongzhi206 <2300652375@qq.com>
2019-09-14 14:25:19 +0800

    Initial Commit

---
+++ pages/homepages/homepages.js
@@ -0,0 +1,73 @@
+// pages/homepages/homepages.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nvalist:[]
+  },
+  getnavlist(){
+    wx.request({
+      url: 'https://easy-mock.com/mock/5c1dfd98e8bfa547414a5278/bili/navList',
+      success(res){
+        console.log("ceshi");
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getnavlist();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
